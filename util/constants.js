21cdc8edcf38a415f6970b1f89363eb53bf7169d
SuperAlex2013 <122688147+SuperAlex2013@users.noreply.github.com>
2023-10-26 13:12:25 +0300

    fix

--- util/constants.js
+++ util/constants.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const DB_DEV = 'mongodb://127.0.0.1:27017/bitfilmsdb';
+const DB_DEV = 'mongodb://localhost:27017/bitfilmsdb';
 const JWT_TOKEN_EXPIRES = '7d';
 
 module.exports = {

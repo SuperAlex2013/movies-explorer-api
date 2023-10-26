7547fd41c84538fe33d45860e983b5fdba3728e0
SuperAlex2013 <122688147+SuperAlex2013@users.noreply.github.com>
2023-10-26 12:33:45 +0300

    fix

--- errors/BadRequestError.js
+++
@@ -1,9 +0,0 @@
-const { BAD_REQUEST } = require('./statusCode');
-
-class BadRequestError extends Error {
-  constructor(msg) {
-    super(msg);
-    this.statusCode = BAD_REQUEST;
-  }
-}
-module.exports = BadRequestError;
